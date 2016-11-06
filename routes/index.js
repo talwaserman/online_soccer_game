83e0a5558e06870c74e9b8333963294fcafdf87d
Tal Waserman <tal.w@fiverr.com>
2016-11-06 11:27:38 +0200

    started working on the main landing page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,23 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Player VS Computer' });
+  res.render('index', { title: 'Welcome to soccer online' });
+});
+
+router.get('/player_vs_computer_same_browser', function(req, res) {
+  res.render('pvc', { title: 'Player VS Computer' });
+});
+
+router.get('/player_vs_player_same_browser', function(req, res) {
+  res.render('pvp', { title: 'Player VS Computer' });
+});
+
+router.get('/player_vs_player_online', function(req, res) {
+  res.render('pvp_online', { title: 'Player VS Player (Online)' });
+});
+
+router.get('/training', function(req, res) {
+  res.render('training', { title: 'training' });
 });
 
 module.exports = router;
---
+++ views/training.handlebars
@@ -0,0 +1,1 @@
+<h1>training</h1>
